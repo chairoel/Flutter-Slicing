3cae67bf3905718039ea51f02ffa15039355b83c
chairul <chairul.amri@ebconnect.com>
2023-04-17 14:27:53 +0700

    Update util border radius selection

--- latihan_slicing_ui/lib/util/util.dart
+++ latihan_slicing_ui/lib/util/util.dart
@@ -3,20 +3,21 @@ import 'package:flutter/cupertino.dart';
 import '../enum/circular_side.dart';
 
 class Util {
-  BorderRadius borderRadiusSelection(CircularSide circularSide, double radius) {
+  BorderRadius borderRadiusSelection(
+      CircularSide circularSide, double radius, double otherRadius) {
     return BorderRadius.only(
       topLeft: (circularSide == CircularSide.topLeft)
           ? Radius.circular(radius)
-          : const Radius.circular(4),
+          : Radius.circular(otherRadius),
       topRight: (circularSide == CircularSide.topRight)
           ? Radius.circular(radius)
-          : const Radius.circular(4),
+          : Radius.circular(otherRadius),
       bottomRight: (circularSide == CircularSide.bottomRight)
           ? Radius.circular(radius)
-          : const Radius.circular(4),
+          : Radius.circular(otherRadius),
       bottomLeft: (circularSide == CircularSide.bottomLeft)
           ? Radius.circular(radius)
-          : const Radius.circular(4),
+          : Radius.circular(otherRadius),
     );
   }
 }
