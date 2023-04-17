986ef478bde9eb669c3b541e90c78056e6ee0010
chairul <chairul.amri@ebconnect.com>
2023-04-17 14:27:22 +0700

    Add new Color variant

--- latihan_slicing_ui/lib/misc/AppColors.dart
+++ latihan_slicing_ui/lib/misc/AppColors.dart
@@ -1,9 +1,12 @@
 import 'dart:ui';
 
+import 'package:flutter/material.dart';
+
 class AppColors {
   static const Color colorBg1 = Color(0xFF171a53);
   static const Color colorBg2 = Color(0xFF142580);
   static const Color colorBg3 = Color(0xFFf6f6f6);
+  static Color colorBg4 = Colors.grey[200]!;
   static const Color colorIcon1 = Color(0xFF213bfe);
   static const Color buttonGreen = Color(0xFFbdea09);
 }
